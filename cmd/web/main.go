5193891e0529ccd93ccfcc801419607cdc7deacd
Lucas Victor <lucas.araujo@ccc.ufcg.edu.br>
2024-06-10 22:05:15 -0300

    new ref

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -71,7 +71,7 @@ func run() (*driver.DB, error) {
 
 	// connect to database
 	log.Println("Connecting to the database...")
-	db, err := driver.ConnectSQL("host=172.19.208.1 port=5432 dbname=bookings-1 user=postgres password=test")
+	db, err := driver.ConnectSQL("host=172.24.80.1 port=5432 dbname=bookings-1 user=postgres password=test")
 	if err != nil {
 		log.Fatal("Cannot connect to database! Dying...")
 	}
