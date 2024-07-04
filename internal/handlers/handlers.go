b600fa1839dee50436c5cf2989f90dd743604139
Lucas Victor <lucas.araujo@ccc.ufcg.edu.br>
2024-07-04 17:37:02 -0300

    created delete handler

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -584,6 +584,19 @@ func (m *Repository) AdminProcessReservation(w http.ResponseWriter, r *http.Requ
 
 }
 
+func (m *Repository) AdminDeleteReservation(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(chi.URLParam(r, "id"))
+	src := chi.URLParam(r, "src")
+
+	log.Println(src)
+
+	_ = m.DB.DeleteReservation(id)
+
+	m.App.Session.Put(r.Context(), "flash", "Reservation deleted")
+	http.Redirect(w, r, fmt.Sprintf("/admin/reservations-%s", src), http.StatusSeeOther)
+
+}
+
 // - HELPERS
 
 func getIntUrlParam(param string, w http.ResponseWriter, r *http.Request) int {
