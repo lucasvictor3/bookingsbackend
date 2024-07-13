27b231318a26fca5001432ac1c417c4e0c1e238e
Lucas Victor <lucas.araujo@ccc.ufcg.edu.br>
2024-07-13 20:19:53 -0300

    removed logs

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -371,7 +371,6 @@ func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
 
 	out, _ := json.MarshalIndent(resp, "", "    ")
 
-	log.Println(string(out))
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(out)
 }
@@ -745,8 +744,6 @@ func (m *Repository) AdminProcessReservation(w http.ResponseWriter, r *http.Requ
 	id, _ := strconv.Atoi(chi.URLParam(r, "id"))
 	src := chi.URLParam(r, "src")
 
-	log.Println(src)
-
 	_ = m.DB.UpdateProcessedForReservation(id, 1)
 
 	year := r.URL.Query().Get("y")
@@ -767,8 +764,6 @@ func (m *Repository) AdminProcessReservation(w http.ResponseWriter, r *http.Requ
 func (m *Repository) AdminDeleteReservation(w http.ResponseWriter, r *http.Request) {
 	id, _ := strconv.Atoi(chi.URLParam(r, "id"))
 	src := chi.URLParam(r, "src")
-
-	log.Println(src)
 
 	_ = m.DB.DeleteReservation(id)
 
