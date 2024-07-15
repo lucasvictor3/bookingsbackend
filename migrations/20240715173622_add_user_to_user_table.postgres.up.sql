69eb649306e1ef16cf4326869d08bdf92b2c463f
Lucas Victor <lucas.araujo@ccc.ufcg.edu.br>
2024-07-15 14:39:28 -0300

    added migration for admin user

---
+++ migrations/20240715173622_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.users (first_name,last_name,email,"password",access_level,created_at,updated_at) VALUES
+	 ('lucas','test','admin@admin.com','$2a$12$BF0l0OGDQhGsfFYwI1WLeuhbpoC8U3y5ATDexBuOUvFF2Iy0lQRFu',3,'2024-06-28 00:00:00.000','2024-06-28 00:00:00.000');
